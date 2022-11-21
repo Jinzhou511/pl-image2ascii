125a31c723c97017db95fbea586daf97f0503bb5
walter <jinzhouwalter@gmail.com>
2022-11-21 02:40:24 -0500

    updated on 11/21

--- imagetoascii.py
+++ imagetoascii.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 from pathlib import Path
+import os
 from argparse import ArgumentParser, Namespace, ArgumentDefaultsHelpFormatter
 
 from chris_plugin import chris_plugin, PathMapper
 from PIL import Image
+#import matplotlib.pyplot as plt
 
 ascii_characters_by_surface = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 __version__ = '1.0.0'
@@ -35,11 +37,13 @@ parser.add_argument(
 
 
 # fucntion part
-def convert_to_ascii_art(inputfile: Path):
+def convert_to_ascii_art(inputfile: Path, outputfile: Path): # PosixPath
     #print("1")
-    #print(inputfile)
-    image = Image.open(inputfile)# Having issue
-    
+
+    image = Image.open(inputfile)
+    (filename, ext) = os.path.splitext(inputfile)
+    #print(filename.split("/")[-1]) 
+    filename = filename.split("/")[-1]
     #print(f"figure format: {image.format}")
     ascii_art = []
     (width, height) = image.size
@@ -49,8 +53,23 @@ def convert_to_ascii_art(inputfile: Path):
             px = image.getpixel((x, y))
             line += convert_pixel_to_character(px)
         ascii_art.append(line)
-    return ascii_art
+    #return ascii_art
+    #print(outputfile)
+    #saving_path = os.path.join(str(outputfile).split("/")[:-1])
+    saving_path = str(outputfile).split(".")[0]
 
+
+    #print(saving_path)
+    
+    with open(saving_path+"_ascii.txt", "w") as file:# 1. modify the name
+
+        for line in ascii_art:
+            file.write(line)
+            file.write('\n')
+        
+        file.close()
+    
+    # 2. change saving path
 
 def convert_pixel_to_character(pixel):
     (r, g, b) = pixel
@@ -60,19 +79,19 @@ def convert_pixel_to_character(pixel):
     index = int(pixel_brightness * brightness_weight) - 1
     return ascii_characters_by_surface[index]
 
-
-def save_as_text(ascii_art):
+'''
+#def save_as_text(ascii_art):
     with open("image.txt", "w") as file:
         for line in ascii_art:
             file.write(line)
             file.write('\n')
         file.close()
 
-
+'''
 # documentation: https://fnndsc.github.io/chris_plugin/chris_plugin.html#chris_plugin
 @chris_plugin(
     parser=parser,
-    title='imageto_ascii',
+    title='image_to_ascii',
     category='',                 # ref. https://chrisstore.co/plugins
     min_memory_limit='2Gi',    # supported units: Mi, Gi
     min_cpu_limit='1000m',       # millicores, e.g. "1000m" = 1 CPU core
@@ -86,12 +105,12 @@ def main(options: Namespace, inputdir: Path, outputdir: Path):
     """
 
     print(DISPLAY_TITLE)
-    mapper = PathMapper.file_mapper(inputdir, outputdir,glob=options.convert)
-    for input in mapper:
+    mapper = PathMapper.file_mapper(inputdir, outputdir, glob = options.convert)
+    for input, output in mapper:
         #print(input)
-        convert_to_ascii_art(input)
-    output_file = outputdir / f'{options.name}.txt'
-    output_file.write_text('did nothing successfully!')
+        convert_to_ascii_art(input, output)
+    #output_file = outputdir / f'{options.name}.txt'
+    #output_file.write_text('did nothing successfully!')
 
 
 if __name__ == '__main__':
